266308e8178cdfc4ad5a732b98cbbaf2d221938b
zision <zision@gmail.com>
2020-07-03 23:45:44 +0100

    level 350 & 400 submission.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // You can edit ALL of the code here
 const allEpisodes = getAllEpisodes();
-
+const allShows = getAllShows();
 function setup() {
   // makePageForEpisodes(allEpisodes);
   display(allEpisodes);
+  displayFetch();
 }
 
 // function makePageForEpisodes(episodeList) {
@@ -91,3 +92,66 @@ function display(array) {
 }
 // ---------------------------------------------------------------------------------------------------------------
 window.onload = setup;
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> DISPLAY FETCH DATA <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+function displayFetch() {
+  fetch("https://api.tvmaze.com/shows/82/episodes ")
+    .then((response) => response.json())
+    .then((data) => fetchData(data));
+}
+
+function fetchData(data) {
+  rootElem.innerHTML = "";
+  data.forEach((episode) => {
+    const sectionMain = document.createElement("section");
+    const h5Els = document.createElement("h5");
+    const pEls = document.createElement("p");
+    const imgEls = document.createElement("img");
+
+    rootElem.appendChild(sectionMain);
+    sectionMain.appendChild(h5Els).textContent = `${
+      episode.name
+    } S${episode.season
+      .toString()
+      .padStart(2, 0)} E${episode.number.toString().padStart(2, 0)}`;
+    sectionMain.appendChild(imgEls).src = episode.image.medium;
+    sectionMain.appendChild(pEls).textContent = episode.summary;
+  });
+}
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+window.onload = setup;
+
+let fetchSelector = document.getElementById("fectchselector");
+let nameArray = allShows.map((i) => i.name);
+
+let sortname = nameArray.sort();
+
+// console.log(sortname);
+
+for (let i = 0; i < nameArray.length; i++) {
+  let fetchel = document.createElement("option");
+  let fetchdisplayOptions = `${nameArray[i]}`;
+
+  fetchel.textContent = fetchdisplayOptions;
+  fetchel.value = `${allShows[i].id}`;
+  fetchSelector.appendChild(fetchel);
+}
+
+fetchSelector.addEventListener("change", (event) => {
+  let fetchselectString = event.target.value;
+  console.log(fetchselectString);
+  fetch(`https://api.tvmaze.com/shows/${fetchselectString}/episodes `)
+    .then((response) => response.json())
+    .then((data) => fetchData(data));
+
+  // let selectFetchFilteredResult = allShows.filter((i) => {
+  //   return (
+  //     ` S${i.season.toString().padStart(2, 0)}E${i.number
+  //       .toString()
+  //       .padStart(2, 0)} - ${i.name}` === fetchselectString
+  //   );
+});
+
+// display(selectFilteredResult);
+// });
