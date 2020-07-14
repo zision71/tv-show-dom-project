e0d57353a7744f9a89b2aea00f2067a2aafbfb44
zision <zision@gmail.com>
2020-07-14 23:50:34 +0100

    submission upto level 500

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const allEpisodes = getAllEpisodes();
 const allShows = getAllShows();
 function setup() {
   // makePageForEpisodes(allEpisodes);
-  display(allEpisodes);
-  displayFetch();
+  displayShowsListing(allShows);
+  // display(allEpisodes);
+  // displayFetch();
 }
 
 // function makePageForEpisodes(episodeList) {
@@ -91,7 +92,6 @@ function display(array) {
   });
 }
 // ---------------------------------------------------------------------------------------------------------------
-window.onload = setup;
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> DISPLAY FETCH DATA <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 function displayFetch() {
@@ -176,15 +176,23 @@ function fetchData(array) {
 }
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-window.onload = setup;
 
+function createButton() {
+  let showel = document.getElementById("showButton");
+  // showel.appendChild(buttonelement);
+
+  showel.style.display = "inline";
+  // showSearchBar.style.display = "none";
+  showel.addEventListener("click", setup);
+}
+
+// ========================================SHOW SELECTOR============================================
 let showSelector = document.getElementById("fectchselector");
 let nameArray = allShows.sort((a, b) => {
   if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
   if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
   return 0;
 });
-console.log(nameArray);
 
 for (let i = 0; i < nameArray.length; i++) {
   let fetchel = document.createElement("option");
@@ -205,4 +213,83 @@ showSelector.addEventListener("change", (event) => {
       .then((data) => fetchData(data));
   }
   displayshowandepisode();
+  createButton();
 });
+
+// *************************************Level 500 Show Listing**************************************************************
+
+function displayShowsListing(array) {
+  searchBar.style.display = "none";
+  selector.style.display = "none";
+  let showel = document.getElementById("showButton");
+  showel.style.display = "none";
+  rootElem.innerHTML = "";
+  array.forEach((episode) => {
+    const sectionMain = document.createElement("section");
+    const h5Els = document.createElement("h5");
+    h5Els.addEventListener("click", () => {
+      let fetchselectString = episode.id;
+
+      console.log(fetchselectString);
+      function displayshowandepisode() {
+        fetch(`https://api.tvmaze.com/shows/${fetchselectString}/episodes `)
+          .then((response) => response.json())
+          .then((data) => fetchData(data));
+      }
+      displayshowandepisode();
+
+      showSelector.style.display = "none";
+      showSearchBar.style.display = "none";
+
+      createButton();
+
+      // buttonEl.style.display = "block";
+      searchBar.style.display = "inline";
+      selector.style.display = "inline";
+      // let showel = document.getElementById("showButton");
+      // showel.appendChild(buttonelement);
+      // showel.style.display = "inline";
+      // showel.addEventListener("click", setup);
+    });
+    showSelector.style.display = "block";
+    showSearchBar.style.display = "inline";
+
+    h5Els.setAttribute("class", "showHeading");
+
+    const pEls = document.createElement("p");
+    const imgEls = document.createElement("img");
+    const h4Els = document.createElement("h4");
+
+    rootElem.appendChild(sectionMain);
+    sectionMain.appendChild(h5Els).innerHTML = `${episode.name} `;
+    if (episode.image !== null) {
+      sectionMain.appendChild(imgEls).src = episode.image.medium;
+    }
+
+    sectionMain.appendChild(pEls).innerHTML = episode.summary;
+    sectionMain.appendChild(
+      h4Els
+    ).innerHTML = `Rated : ${episode.rating.average}<br>
+    Genres: ${episode.genres} <br> Status: ${episode.status}<br> Runtime: ${episode.runtime}`;
+  });
+}
+
+// ***************************************************************************************************************************
+// ==================================================Level 500 step 5=========================================================
+let showSearchBar = document.getElementById("showSearchBar");
+
+showSearchBar.addEventListener("keyup", (e) => {
+  let showSearchstring = e.target.value.toLowerCase();
+
+  let showfilteredResult = allShows.filter((item) => {
+    return (
+      item.name.toLowerCase().includes(showSearchstring) ||
+      item.summary.toLowerCase().includes(showSearchstring) ||
+      item.genres.map((i) => i.toLowerCase()).includes(showSearchstring)
+    );
+  });
+  displayShowsListing(showfilteredResult);
+  showsSearchno.innerText = `Displaying ${showfilteredResult.length}/${allShows.length} Shows`;
+});
+
+window.onload = setup;
